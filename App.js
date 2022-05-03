44ff0ed32636eca560b0bc61fed42307e64cfdee
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-03 15:11:41 -0400

    further project setup and connecting android emulator

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>My canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: 'green',
+          }}
+        >
+          <Text>Search</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            padding: 16,
+            backgroundColor: 'blue',
+          }}
+        >
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
