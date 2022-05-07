71abfe052fe79d730e1e7f883e156bff8d1f4b8c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-06 22:59:47 -0400

    added navigation. Laid foundation for API rendering

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,53 +1,64 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-} from 'react-native';
-import SearchBar from '../components/SearchBar.js';
+import { FlatList, View } from 'react-native';
+import Searchbar from '../components/SearchBar.js';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component.js';
+import { Spacer } from '../../../components/spacer/spacer.component.js';
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import { SafeArea } from '../../../components/utility/safe-area.component';
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]}
   background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
 
-const RestaurantListContainer = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]}
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
-      <SearchBar />
+      <Searchbar />
     </SearchContainer>
-
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
   </SafeArea>
 );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {},
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight,
+//   },
+//   search: {},
+//   list: {
+//     flex: 1,
+//     padding: 16,
+//     backgroundColor: 'blue',
+//   },
+// });
