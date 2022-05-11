bad0e5a32fbe05cbcb07525027dd23c5ed6aad85
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-11 14:25:13 -0400

    optimized mock photos

--- src/components/favorites/favorites-bar.component.js
+++ src/components/favorites/favorites-bar.component.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { ScrollView, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
--- src/services/restaurants/mock/index.js
+++ src/services/restaurants/mock/index.js
@@ -1,21 +1,24 @@
-import antwerp from "./antwerp.json";
-import chicago from "./chicago.json";
-import toronto from "./toronto.json";
-import san_francisco from "./san_francisco.json";
+/* eslint-disable prettier/prettier */
+import antwerp from './antwerp.json';
+import chicago from './chicago.json';
+import toronto from './toronto.json';
+import san_francisco from './san_francisco.json';
 
 export const mocks = {
-  "51.219448,4.402464": antwerp,
-  "43.653225,-79.383186": toronto,
-  "41.878113,-87.629799": chicago,
-  "37.7749295,-122.4194155": san_francisco,
+  '51.219448,4.402464': antwerp,
+  '43.653225,-79.383186': toronto,
+  '41.878113,-87.629799': chicago,
+  '37.7749295,-122.4194155': san_francisco,
 };
 
 export const mockImages = [
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-baking-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291731/website%20pics%20family/pexels-jonathan-borba-2983101_e8ss5r.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291671/website%20pics%20family/pexels-lukas-1352270_zlutws.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291630/website%20pics%20family/pexels-yanuar-putut-widjanarko-2232433_bsa6xx.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291627/website%20pics%20family/pexels-vinicius-benedit-1082343_hihyuj.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291625/website%20pics%20family/pexels-kasumi-loffler-3535383_am1qdk.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652291621/website%20pics%20family/pexels-dana-tentis-725992_xpflb3.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652292255/website%20pics%20family/pexels-%E5%A5%A5%E5%B0%BC%E5%B0%94-%E5%AD%99-2410602_ef5imx.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652292253/website%20pics%20family/pexels-julia-khalimova-3649208_rikwz7.jpg',
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1652292244/website%20pics%20family/pexels-elle-hughes-2647936_e3vupm.jpg',
 ];
