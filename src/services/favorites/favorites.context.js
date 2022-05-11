128cc76183d8f13dc0d2ecd808b4b8e17ab4a6d7
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-11 14:49:30 -0400

    setup persistence for favorites data. Went with local storage, as I have not set up back end yet. The favorites data shouldn't be too large, anyway, so I will probably leave it as a backup cache when backend is storing data. Rendering correctly on ios, currently throwing error in android. Will resolve.

--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,31 @@
-import React, { createContext, useState } from 'react';
+/* eslint-disable prettier/prettier */
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+
+  const saveFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('@favorites', jsonValue);
+    } catch (e) {
+      console.log('error storing', e);
+    }
+  };
+
+  const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@favorites');
+      if (value !== null) {
+        setFavorites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log('error loading', e);
+    }
+  };
 
   const add = (restaurant) => {
     setFavorites([...favorites, restaurant]);
@@ -16,6 +38,15 @@ export const FavoritesContextProvider = ({ children }) => {
 
     setFavorites(newFavorites);
   };
+
+  useEffect(() => {
+    loadFavorites(favorites);
+  }, []);
+
+  useEffect(() => {
+    saveFavorites(favorites);
+  }, [favorites]);
+
   return (
     <FavoritesContext.Provider
       value={{
