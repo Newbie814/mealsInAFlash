ba8c99c7f1876789090ca4370f5a71a3e627fb7c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-16 15:09:51 -0400

    added fade in effect for restaurant card. Cleaned out some unused code

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 import { ActivityIndicator, Colors } from 'react-native-paper';
 
+import { FadeInView } from '../../../components/animations/fade.animation';
 import { SafeArea } from '../../../components/utility/safe-area.component';
 import { Spacer } from '../../../components/spacer/spacer.component';
 import { FavoritesBar } from '../../../components/favorites/favorites-bar.component';
@@ -13,11 +14,7 @@ import { FavoritesContext } from '../../../services/favorites/favorites.context'
 import { Search } from '../components/search.component';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+import { RestaurantList } from '../components/restaurant-list.styles';
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
@@ -58,9 +55,9 @@ export const RestaurantsScreen = ({ navigation }) => {
                 })
               }
             >
-              <Spacer position="bottom" size="large">
+              <FadeInView>
                 <RestaurantInfoCard restaurant={item} />
-              </Spacer>
+              </FadeInView>
             </TouchableOpacity>
           );
         }}
