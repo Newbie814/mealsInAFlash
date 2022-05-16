59fb8a3896425edc4fa907cbff37f039cb6e7283
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-15 23:59:05 -0400

    built rudimentary logout function. fixed favorties to unique user id's - they were persisting across everyone.

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -11,6 +11,10 @@ import { MapScreen } from '../../features/map/screens/map.screen';
 
 import { AuthenticationContext } from '../../services/authentication/authentication.context';
 
+import { RestaurantsContextProvider } from '../../services/restaurants/restaurants.context';
+import { LocationContextProvider } from '../../services/location/location.context';
+import { FavoritesContextProvider } from '../../services/favorites/favorites.context';
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
@@ -39,15 +43,21 @@ const createScreenOptions = ({ route }) => {
 };
 
 export const AppNavigator = () => (
-  <Tab.Navigator
-    screenOptions={createScreenOptions}
-    tabBarOptions={{
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    }}
-  >
-    <Tab.Screen name='Restaurants' component={RestaurantsNavigator} />
-    <Tab.Screen name='Map' component={MapScreen} />
-    <Tab.Screen name='Settings' component={Settings} />
-  </Tab.Navigator>
+  <FavoritesContextProvider>
+    <LocationContextProvider>
+      <RestaurantsContextProvider>
+        <Tab.Navigator
+          screenOptions={createScreenOptions}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name='Restaurants' component={RestaurantsNavigator} />
+          <Tab.Screen name='Map' component={MapScreen} />
+          <Tab.Screen name='Settings' component={Settings} />
+        </Tab.Navigator>
+      </RestaurantsContextProvider>
+    </LocationContextProvider>
+  </FavoritesContextProvider>
 );
